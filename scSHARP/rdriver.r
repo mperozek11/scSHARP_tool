67403c674c3552b2d3933b5cc5561ad7e7052914
Benjamin Modlin <82414516+bwmodlin@users.noreply.github.com>
2023-04-16 14:41:51 -0600

    Update documentation on rdriver.r

--- scSHARP/rdriver.r
+++ scSHARP/rdriver.r
@@ -1,11 +1,12 @@
+# This script connects our python package to our R package
+
+# loading our R package
 library(R4scSHARP)
 
+# read command line arguments
 args = commandArgs(trailingOnly=TRUE)
 
-# input vars:
-# paths should be relative to your working directory
-# (call getwd() to get this information).
-
+# get necessary paths from python package
 data_path <- args[1]
 out_path <- args[2]
 marker_path <- args[3]
@@ -15,6 +16,6 @@ tools <- args[6]
 
 print(out_path)
 
-
+# run R tools
 output <- run_r4scsharp(data_path, marker_path,
     ref_path, ref_label_path, out_path=out_path, tools=tools)
