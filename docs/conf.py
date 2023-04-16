1142a756f9885753eea7d4dc2029c17e99d19a2d
maxperozek <maxperozek@gmail.com>
2023-04-16 15:56:28 -0600

    few

--- docs/conf.py
+++ docs/conf.py
@@ -2,15 +2,6 @@ import os, sys
 # sys.path.append(os.path.abspath('/Users/maxperozek/CP499/scSHARP_tool'))
 # sys.path.append(os.path.relpath('scSHARP_tool'))
 
-# Get the directory of the current script
-script_dir = os.path.dirname(os.path.abspath(__file__))
-print(script_dir)
-# Construct the relative path from the current script directory
-relative_path = os.path.join(script_dir, 'CP499', 'scSHARP_tool')
-
-# Append the relative path to the system path
-sys.path.append(relative_path)
-
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
